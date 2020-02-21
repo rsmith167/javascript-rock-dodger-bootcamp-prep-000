d4a5b9f889b46dd90842c737a99dbac8fcf55b46
Rwanda S <rwanda.ace@gmail.com>
2020-02-21 19:03:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -139,9 +139,10 @@ function createRock(x) {
   // Add the rock to ROCKS so that we can remove all rocks
   // when there's a collision.
   ROCKS.push(rock);
-  console.log(ROCKS[0])
+  
   // Finally, return the rock element you've created.
   return rock;
+  console.log(rock)
 }
 
 function moveDodger(e) {
